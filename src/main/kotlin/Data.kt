db56cf24658aa2b137285c587285cf367490a0fc
oh980225 <oh980225@gmail.com>
2022-04-04 21:35:35 +0900

    [내용 수정] 코틀린 2강 (Data Class)

--- src/main/kotlin/Data.kt
+++ src/main/kotlin/Data.kt
@@ -1,11 +1,10 @@
 import java.time.LocalDate
 
-// get 메서드, toString(), hashCode(), equals(), copy() 가 기본적으로 만들어짐
+// toString(), hashCode(), equals(), copy() 가 기본적으로 만들어짐
 // 관련된 data class를 하나의 파일에 모아놓을 수도 있음
-data class Ticket(val companyName: String, val name: String, var date: LocalDate, var seatNumber: Int)
+data class Ticket(private val companyName: String, val name: String, var date: LocalDate, var seatNumber: Int)
 
 fun main() {
   val ticketA = Ticket("대한 항공", "Seungjae", LocalDate.now(), 15)
-  println(ticketA.date)
   println(ticketA)
 }
