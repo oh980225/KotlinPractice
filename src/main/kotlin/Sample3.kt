d834b67f1c007097b93fe547db3765ff0e1db57f
oh980225 <oh980225@gmail.com>
2022-04-04 21:48:20 +0900

    코틀린 2강 (Companion)

--- src/main/kotlin/Sample3.kt
+++ src/main/kotlin/Sample3.kt
@@ -1,2 +1,26 @@
-class Sample3 {
+class Book private constructor(val id: Int, val name: String) {
+  // companion object -> private property나 method를 불러올 수 있게 해줌!
+  //  companion는 class 내부에 Companion이라는 정적(static) 객체를 생성
+  companion object BookFactory : IdProvider {
+    val myBook = "new book"
+
+    fun create() = Book(getId(), "animal farm")
+
+    override fun getId(): Int {
+      return 5
+    }
+  }
+}
+
+interface IdProvider {
+  fun getId(): Int
+}
+
+fun main() {
+  // Book.Companion.create() -> Book.create() / Companion 생략 가능
+  val book = Book.create()
+
+  val booId = Book.BookFactory.getId()
+
+  println("${book.id} ${book.name}")
 }
